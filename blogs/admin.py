909a2812c81b348fd1837991a1cff354bd97e3d1
Nitin Bopche <nikbopche125256@gmail.com>
2025-12-16 16:50:35 +0530

    blogs app and blog model

---
+++ blogs/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Category, Blog
+# Register your models here.
+
+
+
+class Blogadmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)} 
+    list_display= ('title', 'category', 'author', 'status', 'is_featured', 'created_at')
+    search_fields = ('id', 'title', 'category__category_name', 'status')
+    list_editable=('is_featured',)
+
+admin.site.register(Category)
+
+admin.site.register(Blog, Blogadmin)
+ 
--- vlog_main/settings.py
+++ vlog_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs.apps.BlogsConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
