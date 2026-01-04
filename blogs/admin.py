8361a8ebc29c0131b9ed15e3ff3a8353591bd183
Nitin Bopche <nikbopche125256@gmail.com>
2026-01-04 13:50:26 +0530

    comment feature

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Blog
+from .models import Category, Blog , Comment
 # Register your models here.
 
 
@@ -13,4 +13,5 @@ class Blogadmin(admin.ModelAdmin):
 admin.site.register(Category)
 
 admin.site.register(Blog, Blogadmin)
- 
+
+admin.site.register(Comment) 
--- blogs/models.py
+++ blogs/models.py
@@ -40,3 +40,16 @@ class Blog(models.Model):
 
     def __str__(self):  
         return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)# on delete user delete comment    
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    comment= models.TextField(max_length=500) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)   
+
+
+
+    def __str__(self):
+        return self.comment
