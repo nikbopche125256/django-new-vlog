694e6061372cff3144bf3b03bfa09306aa2d2078
Nitin Bopche <nikbopche125256@gmail.com>
2026-01-04 12:51:14 +0530

    manage users functionality for managers

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from blogs.models import Category, Blog
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
@@ -9,3 +11,17 @@ class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ('title', 'category', 'featured_image', 'short_description', 'blog_body', 'status', 'is_featured')
+
+
+class AddUserForm(UserCreationForm):# Inheriting UserCreationForm for user creation it comes with password field default
+    class Meta:
+        model = User
+        fields =  ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+
+        # here we not defined the field password but by using UserCreationForm it will automatically show this field
+
+
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields =  ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')        
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect , get_object_or_404
 from blogs.models import Category , Blog
-from .forms import CategoryForm ,BlogPostForm
+from .forms import CategoryForm ,BlogPostForm , AddUserForm, EditUserForm
 from django.contrib.auth.decorators import login_required
 from django.template.defaultfilters import slugify
-
+from django.contrib.auth.models import User 
 # Create your views here.
 @login_required(login_url='login')
 def dashboard(request):
@@ -113,3 +113,51 @@ def delete_post(request, pk):
     post = get_object_or_404(Blog, pk=pk)
     post.delete()
     return redirect('posts')
+
+
+def users(request):
+    users= User.objects.all()
+    context={
+        'users': users,
+    }
+    return render(request, 'dashboard/users.html', context)
+
+
+def add_user(request):
+    if request.method == 'POST':
+        form = AddUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+        else:
+            print('form invalid')  
+            print(form.errors)    
+    form= AddUserForm()
+    context={
+        'form': form,
+    }
+
+    return render(request, 'dashboard/add_user.html', context)
+
+
+def edit_user(request, pk):
+    user=get_object_or_404(User, pk=pk)
+    if request.method =='POST':
+        form = EditUserForm(request.POST, instance=user)# instance to update existing user
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+        else:
+            print('form invalid')  
+            print(form.errors)    
+    form=EditUserForm(instance=user)
+    context={
+        'form': form,   
+    }
+    return render(request, 'dashboard/edit_user.html', context)    
+
+
+def delete_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    user.delete()
+    return redirect('users')
